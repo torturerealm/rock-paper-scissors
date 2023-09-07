581ed5ce88b8c43df5a31953ede2e39aec3910f4
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-07 16:45:12 +0800

    Turn prompt to lowercase so it is case insensitive

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function getComputerChoice() {
-    // randomly generate a number from 1-3
-    // if 1, rock
-    // if 2, paper
-    // if 3, scissors
-    
     let random = Math.floor(Math.random() * 3) + 1;
     if (random === 1) {
         return "rock";
@@ -17,30 +12,35 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) return "It's a draw!";
-
-    // Player wins
-    // if playerSelection is rock and computerSelection is scissors
-        // return "You win! Rock beats Scissors"
-    // if playerSelection is paper and computerSelection is rock
-        // return "You win! Paper beats Rock"
-    // if playerSelection is scissors and computerSelection is paper
-        // return "You win! Scissors beat Paper"
-
-    // Player lose
-    // if playerSelection is rock and computerSelection is paper
-        // return "You lose! Paper beats Rock"
-    // if playerSelection is paper and computerSelection is scissors
-        // return "You lose! Scissors beat Paper"
-    // if playerSelection is scissors and computerSelection is rock
-        // return "You lose! Rock beats Scissors"
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats Scissors";
+        }
+        else if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beat Paper";
+        }
+        else if (computerSelection === "rock") {
+            return "You win! Paper beats Rock";
+        }
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beat Paper";
+        }
+        else if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors";
+        }
+    }
 }
 
-// Let playerSelection store lowercase prompt from user
-// Let computerSelection store input from getComputerChoice function
-// Take in playerSelection and ComputerSelection as parameters for playRound
-// playRound function alerts the user
-
-let playerSelection = prompt("Rock, Paper, or Scissors? ", "");
+let playerSelection = prompt("Rock, Paper, or Scissors? ", "").toLowerCase();
 let computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection))
